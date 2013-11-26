a4ab67bd460ac655a3e733780805cfa234fd06f0
neerajsohal <hello@neerajkumar.name>
2013-11-26 18:29:29 +0530

    adding config.php for travis

---
+++ tests/config.php
@@ -0,0 +1,15 @@
+<?php 
+
+/**
+ * Configuration for running tests
+ * 
+ */
+
+namespace AmplifierTests;
+
+$config = array(
+	'app_id' => '462418160544951',
+	'secret' => '437c3981b9af50c5f4befaf4a9fb91cf',
+	'access_token' => 'CAAGkkR4ybLcBACJ9Na6ZByZCP8BnemMiTXO6wunOZBPId9HjG4YEPZAZByWAKLyqh4VEZAkVZBe5xAkeDIaWMZBZBxOtrJoNKCtPZAFlAdj6Gc12pHv4SYIu39YrghKWGlOSHofaCiDRGrCzg4ImlLixdd2XC1f7wm6TJTccz9ZBhCRp2Uf3WEA08i2T9LJczcMeJsZD',
+	'test_page_id' => '107416622618393'
+);
