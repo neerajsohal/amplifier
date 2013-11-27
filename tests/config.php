0b70b05d0b9176a13b89940689ef9144d79ae656
neerajsohal <hello@neerajkumar.name>
2013-11-27 19:06:34 +0530

    changing access token

--- tests/config.php
+++ tests/config.php
@@ -10,6 +10,6 @@ namespace AmplifierTests;
 $config = array(
 	'app_id' => '462418160544951',
 	'secret' => '437c3981b9af50c5f4befaf4a9fb91cf',
-	'access_token' => 'CAAGkkR4ybLcBACJ9Na6ZByZCP8BnemMiTXO6wunOZBPId9HjG4YEPZAZByWAKLyqh4VEZAkVZBe5xAkeDIaWMZBZBxOtrJoNKCtPZAFlAdj6Gc12pHv4SYIu39YrghKWGlOSHofaCiDRGrCzg4ImlLixdd2XC1f7wm6TJTccz9ZBhCRp2Uf3WEA08i2T9LJczcMeJsZD',
+	'access_token' => 'CAAGkkR4ybLcBAFMg0i6AuTXAqBOwP24Vxl6Ofw9rorPCHHXSrMhFYZAwUmtCS9XnNFb4ZB6FdCmy2jCxB826NLQNnHb1u0ZCWoPITIPmTXxkTTtCtqKGwyZA7LjC0ea0a0BrLDVwuGq5CPj35CRwU0uwRRm3WnPSuJNOPH3qDPCfgb6IxwnhAbYMxSo0LyIZD',
 	'test_page_id' => '107416622618393'
 );
