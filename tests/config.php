f535f72ead51af1970f96039dbd0f518d0f3e9dd
neerajsohal <hello@neerajkumar.name>
2013-11-29 23:58:14 +0530

    adding php version check

--- tests/config.php
+++ tests/config.php
@@ -10,6 +10,6 @@ namespace AmplifierTests;
 $config = array(
 	'app_id' => '462418160544951',
 	'secret' => '437c3981b9af50c5f4befaf4a9fb91cf',
-	'access_token' => 'CAAGkkR4ybLcBAFMg0i6AuTXAqBOwP24Vxl6Ofw9rorPCHHXSrMhFYZAwUmtCS9XnNFb4ZB6FdCmy2jCxB826NLQNnHb1u0ZCWoPITIPmTXxkTTtCtqKGwyZA7LjC0ea0a0BrLDVwuGq5CPj35CRwU0uwRRm3WnPSuJNOPH3qDPCfgb6IxwnhAbYMxSo0LyIZD',
+	'access_token' => 'CAAGkkR4ybLcBAPUYUAmaiSJIhEboKqJmKBQ1g7FDyVAeSmbIZBeWhx0ppfK8ZBh59DWbtgv5UVG2xk8M6RxFykCSEi9ZAuFU7xvUKfkgiRxXdR50z83BBkfHcjCyLqdvxUn2PuPoMMNa6ZARRGTyjkFOnKnNr1xpPhvYJtSFJPxbTm2Q4dHLQoODCadxWDsZD',
 	'test_page_id' => '107416622618393'
 );
