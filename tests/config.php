c46153f1a2df1884dfcbc2859a1e094a2b317caf
neerajsohal <hello@neerajkumar.name>
2013-12-03 14:12:53 +0530

    adding new method, adding license

--- tests/config.php
+++ tests/config.php
@@ -10,6 +10,6 @@ namespace AmplifierTests;
 $config = array(
 	'app_id' => '462418160544951',
 	'secret' => '437c3981b9af50c5f4befaf4a9fb91cf',
-	'access_token' => 'CAAGkkR4ybLcBAPUYUAmaiSJIhEboKqJmKBQ1g7FDyVAeSmbIZBeWhx0ppfK8ZBh59DWbtgv5UVG2xk8M6RxFykCSEi9ZAuFU7xvUKfkgiRxXdR50z83BBkfHcjCyLqdvxUn2PuPoMMNa6ZARRGTyjkFOnKnNr1xpPhvYJtSFJPxbTm2Q4dHLQoODCadxWDsZD',
+	'access_token' => 'CAAGkkR4ybLcBALDSZCmngkaG4lyXBFaVL3tBfb8NxB8d0wWqFRTsmfX3PpQgHVad69hxaJZAcDYbe632A7GZBf9lED5czwoO9y1YhlfDx88cgpvxmO7XqSuJDWKIyNt7EUYoayXVwCmbblAmr6d2OZBfc6vuL4Vef06uzK09L3CeHlUbXD0ci4q9gflZAz50ZD',
 	'test_page_id' => '107416622618393'
 );
